54f4d935731f3c11b03f6243d2b3751e024a53fe
Alison Christian P. A. de Carvalho <62998742+AlisonChs@users.noreply.github.com>
2020-12-04 12:19:57 -0300

    Funções movidas para um arquivo JS mais organizado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,38 @@
+function mudarBotaoObg() {
+	document.getElementById('botaoS').setAttribute('class', 'botao-sucesso');
+	document.getElementById('botaoS').innerHTML = "Obrigado!"
 
-		var posicao = 0;
-		var mensagem = "Sua mensagem aqui      <br>É incrível, não é?       <br>Chame a atenção do usuário      <br>Indique <a href='#'>links</a> para ele.                  ";  // Digite entre as aspas o que você quiser, inclusive tags HTML.
+	document.getElementById('botaoSG').setAttribute('class', 'botaoS-sucesso');
+	document.getElementById('botaoSG').innerHTML = "Obrigado!"
+}
+
+function mudarModal() {
+	document.getElementById('modal-donate-js').setAttribute('class', 'modal-dialog modal-dialog-centered modal-dialog-scrollable')
+}
+
+function mudarBotaoDonate() {
+
+	// document.getElementById('botaoS').setAttribute('class', 'botao-sucesso');
+	document.getElementById('botaoD').innerHTML = "Escolha uma opção"
+}
+
+function esc(el) {
+	var display = document.getElementById(el).style.display;
+	if(display == "none")
+		document.getElementById(el).style.display = 'block';
+	else
+		document.getElementById(el).style.display = 'none';
+}
+
+var posicao = 0;
+		var mensagem = "< O futuro é agora.     <br>Você precisa de inovação.       <br>Você precisa de controle.>      <br><a href='#'>Contribua</a> ou faça um orçamento. <br></>                 ";  // Digite entre as aspas o que você quiser, inclusive tags HTML.
   
-		function rola() {
+		function efeito() {
 		  document.getElementById("mensagem-inicio").innerHTML = mensagem.substring(posicao,mensagem,length);
 		  posicao++;
 		  if (posicao == mensagem.length) {
 	posicao = 0;
 		  }
-		  setTimeout("rola()", 130); // O número mencionado pode ser alterado, pois ele indica a velocidade com que a mensagem irá surgir. Quanto maior, mais lento.
+		  setTimeout("efeito()", 130);
 		}
   
