1426a50c34acc694f88fa2add04bd8ea5151d97c
Alison Christian P. A. de Carvalho <62998742+AlisonChs@users.noreply.github.com>
2020-12-17 09:31:52 -0300

    Criação da pasta ''projects'' e testes

---
+++ projects/devflix/precache-manifest.b3a937d1b74fae6fc42f27c43ad8756a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0caa8ef936e7420deaa0230b12733e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9a47708120f548d3dd5",
+    "url": "/static/css/2.772ba614.chunk.css"
+  },
+  {
+    "revision": "42e778d51d4552b00379",
+    "url": "/static/css/main.b3780ff1.chunk.css"
+  },
+  {
+    "revision": "d9a47708120f548d3dd5",
+    "url": "/static/js/2.2cceb7b1.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.2cceb7b1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "42e778d51d4552b00379",
+    "url": "/static/js/main.fd60c80f.chunk.js"
+  },
+  {
+    "revision": "a16fd907a56b6991363f",
+    "url": "/static/js/runtime-main.a75c6258.js"
+  },
+  {
+    "revision": "f2c23aac0743e4341cfde958530ed69c",
+    "url": "/static/media/banner.f2c23aac.png"
+  },
+  {
+    "revision": "65abfa1ded9312e0f744c65a7f365b05",
+    "url": "/static/media/logo.65abfa1d.png"
+  }
+]);
